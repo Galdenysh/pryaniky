2fc0f79da27ff1e09d26291a807eaad4086bd9a2
Igor Petrov <galdenysh@MacBook-Air-Igor.local>
2023-03-15 23:13:13 +0300

    refactor: fix api funcs

--- src/utils/api-pryaniki.ts
+++ src/utils/api-pryaniki.ts
@@ -1,8 +1,14 @@
 import { HOST } from "./constants";
 import { getWithExpiry } from "./localstorage";
-import { ILoginData, IResponseData } from "./types";
+import { ILoginData } from "./types";
 
-const checkResponse = (res: Response) => {
+const headersContentType = { 'Content-Type': 'application/json' };
+const headersAuthorization = () => ({
+  'Content-Type': 'application/json',
+  'x-auth': `${getWithExpiry('auth_token')}`,
+});
+
+function checkResponse(res: Response) {
   if (res.ok) return res.json();
   return Promise.reject(`Ошибка ${res.status}`);
 };
@@ -12,14 +18,8 @@ function request(endpoint: string, options: RequestInit) {
   return fetch(`${HOST}${endpoint}`, options).then(checkResponse);
 }
 
-const headersContentType = { 'Content-Type': 'application/json' };
-export const headersAuthorization = () => ({
-  'Content-Type': 'application/json',
-  'x-auth': `${getWithExpiry('auth_token')}`,
-});
-
 // Login
-export const singIn = (data: ILoginData) => {
+export function singIn(data: ILoginData) {
   const options = {
     method: 'POST',
     headers: headersContentType,
@@ -29,8 +29,8 @@ export const singIn = (data: ILoginData) => {
   return request('/ru/data/v3/testmethods/docs/login', options);
 };
 
-// GetData
-export const getData = () => {
+// Получение данных для карточек
+export function getData() {
   const options = {
     method: 'GET',
     headers: headersAuthorization(),
